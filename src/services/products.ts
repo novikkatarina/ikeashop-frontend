998f0d8a32060d786c161ee2164d796490178d26
Katarina Novikova <novikkatarina@gmail.com>
2023-10-16 12:27:16 +0400

    Fix url

--- src/services/products.ts
+++ src/services/products.ts
@@ -4,7 +4,7 @@ import { IGetProductsResponse } from 'models';
 export const getProducts = async () => {
   let response: IGetProductsResponse;
 
-  response = await axios.get('http://195.2.85.240l/Product/GetProducts');
+  response = await axios.get('http://195.2.85.240/Product/GetProducts');
   const products = response.data || [];
 
   return products;
